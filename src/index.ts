4ddb4eaee28b63d589bcf1bbf49d477208008022
Jeff Mangan <jeff@magentatech.com>
2023-05-15 13:38:39 -0600

    updated default route

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
 import express from 'express';
+import path from 'path';
 import {
   createUser,
   getUserById,
   updateUser,
   deleteUser,
 } from './infrastructure/api/UserController';
+import { hostname } from 'os';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
-app.use(express.json());
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin(s) allowed to access your API
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.post('/users', createUser);
 app.get('/users/:id', getUserById);
 app.put('/users/:id', updateUser);
 app.delete('/users/:id', deleteUser);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on port ${HOST}:${PORT}`);
 });
